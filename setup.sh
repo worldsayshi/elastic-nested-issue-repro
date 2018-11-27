16cbcdbe548fa583d5afda15a4304f09f03f2984
Per Fredelius <per.fredelius@findwise.com>
2018-11-27 16:03:04 +0100

    cleanup

--- setup.sh
+++ setup.sh
@@ -12,12 +12,6 @@ curl -H 'Content-Type: application/json' -XPUT http://localhost:9200/integration
     }
 }'
 
-# sh enable_requestlog.sh
-
-# curl -H 'Content-Type: application/json' -XPOST http://localhost:9200/integration_test_index_de/_flush
-#
-# POST twitter/
-
 curl -H 'Content-Type: application/json' -XPUT http://localhost:9200/integration_test_index_de/default/test_document?refresh=true -d@document.json
 
 printf "\n"
