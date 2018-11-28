e13e26ed4ec33ba9ee907512310c73490820d161
Per Fredelius <per.fredelius@findwise.com>
2018-11-28 10:54:28 +0100

    Removed non-essential script

--- enable_requestlog.sh
+++
@@ -1,5 +0,0 @@
-curl -H 'Content-Type: application/json' -XPUT 'http://localhost:9200/integration_test_index_*/_settings?preserve_existing=true' -d '{
-  "index.indexing.slowlog.threshold.index.debug" : "0s",
-  "index.search.slowlog.threshold.fetch.debug" : "0s",
-  "index.search.slowlog.threshold.query.debug" : "0s"
-}'
